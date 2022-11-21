33e0f34acf22a1317de35683ccf0a40cf72d415f
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2022-11-22 00:21:23 +0700

    Add files via upload

---
+++ src/pages/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return (
+    <div>
+      <h1 style={{textAlign: "center", color: "red",marginTop:"20%",fontSize: "100px"}}>404 NOT FOUND</h1></div>
+    
+  )
+}
+
+export default NotFound
